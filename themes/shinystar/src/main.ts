6dd241389ce1f5040106dfced6ec1373bc0dab69
vincent <60036604+LittleSwift@users.noreply.github.com>
2024-10-05 16:41:33 +0800

    Should be valid againnn

--- themes/shinystar/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import './assets/global.css';
-import router from "./router.ts";
-import App from "./components/App.vue";
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
